f27fc53d48b49d5b68971a6957bf24c205679467
ZhiLeong <saber_zhileong@hotmail.com>
2022-11-13 01:52:44 -0500

    commit tree dbaf28af24be044661e7bf867ba268cc5a659508

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let employee={name:'Sam'}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let employees={...employee}
+    employees[key] = value
+    return employees
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    let cloneofemployee = {...employee}
+    delete cloneofemployee[key]
+    return cloneofemployee
+   
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
